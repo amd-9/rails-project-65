9fcd3093b110d6594af0a3e7cd21c64e062e3baa
Anton Samoshin <asamoshin9@gmail.com>
2023-05-25 22:33:57 +0300

    Add auth logic and tests

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class Web::AuthController < ApplicationController
+  def callback
+    user_info = request.env['omniauth.auth']
+
+    user_data = user_info[:info]
+
+    @user = User.new(name: user_data.name, email: user_data.email)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to auth_request_path
+    else
+      redirect_to auth_request_path, alert: t('.auth.create.fail')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post 'auth/:provider', to: 'auth#request', as: :auth_request
-  get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+  scope module: :web do
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+  end
 end
