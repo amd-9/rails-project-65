9cb1bb22fa103d1f312ea8b2e3bae5ef08444fa6
Anton Samoshin <asamoshin9@gmail.com>
2025-07-30 15:41:01 +0300

    Add OmniAuth

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  post 'auth/:provider', to: 'auth#request', as: :auth_request
+  get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
 end
