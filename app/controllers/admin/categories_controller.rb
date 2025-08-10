b487d4088dabe9b0acc7c7143800adbd389fe6fe
Anton Samoshin <asamoshin9@gmail.com>
2025-08-10 14:35:05 +0300

    Add Categories controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,45 @@
+class Admin::CategoriesController < ApplicationController
+    def index
+        @categories = Category.order(created_at: :desc)
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            redirect_to categories_path
+        else
+             render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def create
+        @category = Category.build(category_params)
+
+        if @category.save
+            redirect_to categories_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+
+        redirect_to categories_path
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,10 @@ Rails.application.routes.draw do
     # post "auth/:provider", to: "auth#request", as: :auth_request
     get "auth/:provider/callback", to: "auth#callback", as: :callback_auth
   end
+
+  scope module: "admin" do
+    resources :categories, only: %i[index new edit update create destroy]
+
+    get "index", to: "categories#index"
+  end
 end
