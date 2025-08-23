cc6fc361f43908738beb14ee19d15aa9e3da6289
Anton Samoshin <asamoshin9@gmail.com>
2025-08-23 16:44:54 +0300

    Add logout test

--- test/controllers/web/auth_controller_test.rb
+++ test/controllers/web/auth_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Web::AuthControllerTest < ActionDispatch::IntegrationTest
   test "check github auth" do
-    post auth_request_path("github")
+    post "/auth/github"
     assert_response :redirect
   end
 
@@ -25,5 +25,29 @@ class Web::AuthControllerTest < ActionDispatch::IntegrationTest
 
     assert user
     assert signed_in?
+  end
+
+  test "logout" do
+    auth_hash = {
+      provider: "github",
+      uid: "12345",
+      info: {
+        email: Faker::Internet.email,
+        name: Faker::Name.first_name
+      }
+    }
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
+
+    get callback_auth_url("github")
+    assert_response :redirect
+
+    user = User.find_by(email: auth_hash[:info][:email].downcase)
+
+    assert user
+    assert signed_in?
+
+    get auth_logout_path
+    assert !signed_in?
   end
 end
