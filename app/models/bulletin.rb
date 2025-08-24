05b7a39b997a1869edc64f34d43c6ecd0cbd3059
Anton Samoshin <asamoshin9@gmail.com>
2025-08-24 11:10:51 +0300

    Add bulletin creator presence validation

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -6,6 +6,7 @@ class Bulletin < ApplicationRecord
     validates :title, presence: true, length: { maximum: 50 }
     validates :description, length: { maximum: 1000 }
     validates :image, attached: true, content_type: %i[png jpg jpeg], size: { less_than: 5.megabytes }
+    validates :user, presence: true
 
     belongs_to :category
     belongs_to :creator, class_name: "User", foreign_key: :user, inverse_of: :bulletins
