984836d8ecb74df33f4bb90c1073b261cfc09dfd
Anton Samoshin <asamoshin9@gmail.com>
2025-09-05 09:01:54 +0300

    Add bulletin description presence validation

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -6,7 +6,7 @@ class Bulletin < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true, length: { maximum: 50 }
-  validates :description, length: { maximum: 1000 }
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :image, attached: true, content_type: %i[png jpg jpeg], size: { less_than: 5.megabytes }
 
   belongs_to :category
