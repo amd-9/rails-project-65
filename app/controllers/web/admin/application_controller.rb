e01a42b59f5ab7d980de7907cf79d2dbc0b139b5
Anton Samoshin <asamoshin9@gmail.com>
2025-09-20 13:09:18 +0300

    Rename admin check method in admin application controller

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Web::Admin::ApplicationController < Web::ApplicationController
-  before_action :authorize_user
+  before_action :authorize_admin
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def authorize_user
+  def authorize_admin
     authorize :user, :admin?
   end
 end
