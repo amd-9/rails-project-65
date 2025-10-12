3bb4a84229fdcc925cb84ddcb33f949a5b3ba047
Anton Samoshin <asamoshin9@gmail.com>
2025-10-12 18:13:57 +0300

    Move OTEL initializer to only production environment

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-require 'opentelemetry/sdk'
-require 'opentelemetry/exporter/otlp'
-require 'opentelemetry/instrumentation/all'
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'Awesome_Bulletins'
-  c.use_all # enables all instrumentation!
+if Rails.env.production?
+  require 'opentelemetry/sdk'
+  require 'opentelemetry/exporter/otlp'
+  require 'opentelemetry/instrumentation/all'
+  OpenTelemetry::SDK.configure do |c|
+    c.service_name = 'Awesome_Bulletins'
+    c.use_all # enables all instrumentation!
+  end
 end
